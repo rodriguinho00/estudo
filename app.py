4ba17369e8951e9ba7b06f04fc698c8d15e4d12a
Rodrigo Santos <rodrigosantos5510jau@gmail.com>
2026-03-31 15:26:11 -0300

    incluido atualizaçoes do projeto

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
 from flask import Flask, render_template, request
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['get',  'post'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     
- conta = None
- pessoas = None
- gorjeta = None
+    if request.method == 'POST':  
 
- if request.method == 'post':
-  conta = request.form['conta']
-  pessoas = request.form['pessoas']
-  gorjeta = request.form['gorjeta']
+        conta = float(request.form['conta'])
+        pessoas = int(request.form['pessoas'])
+        gorjeta = float(request.form['gorjeta'])
 
- print(conta, pessoas, gorjeta)
+        valor_gorjeta = conta * (gorjeta / 100)
+        total = conta + valor_gorjeta
+        por_pessoa = total / pessoas
 
- return render_template('index.html')
+        if gorjeta < 5:
+            classificacao = "Mão de vaca"
+        elif gorjeta <= 15:
+            classificacao = "Legal"
+        else:
+            classificacao = "Generoso"
+
+        return render_template(
+            'resultado.html',
+            gorjeta=valor_gorjeta,
+            total=total,
+            por_pessoa=por_pessoa,
+            classificacao=classificacao
+        )
+
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.run(debug=True) 
+    app.run(debug=True)
